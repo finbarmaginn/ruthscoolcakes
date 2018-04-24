0affd26659a45ededdfb0576b26e4dc17c683334
finbarmaginn <finbarmaginn@gmail.com>
2018-04-24 17:12:03 +0100

    sitemap

---
+++ src/Router.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+export default(<Route>
+  <Route path="/"></Route>
+  <Route path="/gallery"></Route>
+  <Route path="/flavours"></Route>
+  <Route path="/reviews"></Route>
+  <Route path="/contact"></Route>
+</Route>);
